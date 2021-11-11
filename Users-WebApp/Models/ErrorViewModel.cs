52f84493a8e06b0cf7ff72aaf008db7d77736e5e
JOSEPH STAVERS <W9109554@live.tees.ac.uk>
2021-11-11 12:18:34 +0000

    Add project files.

---
+++ Users-WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Users_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
